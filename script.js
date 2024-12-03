09c3b465330d2f86d04b3f86c4b7c05beac5382c
bbantier <65014410+bbantier@users.noreply.github.com>
2024-12-03 12:13:26 +0100

    add functionality to each button

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const myLibrary = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pages: 295,
-    read: true,
-  },
-];
+const myLibrary = [];
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,9 +16,16 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function updateUi() {
+  const bookTable = document.querySelector(".book-list");
+  const bookRows = document.querySelectorAll(".book-row");
+
+  bookRows.forEach((element) => {
+    element.remove();
+  });
+
   myLibrary.forEach((book) => {
-    const bookTable = document.querySelector(".book-list");
     const bookRow = document.createElement("tr");
+    bookRow.classList.add("book-row");
     bookRow.innerHTML += `
       <td>${book.title}</td>
       <td>${book.author}</td>
@@ -36,18 +40,32 @@ document.addEventListener("DOMContentLoaded", () => updateUi());
 
 const newBookButton = document.querySelector("#new-book-button");
 const newBookModal = document.querySelector("#new-book-modal");
+const addButton = document.querySelector("#add-button");
 const cancelButton = document.querySelector("#cancel-button");
 
 newBookButton.addEventListener("click", () => {
   newBookModal.style.display = "block";
-})
+});
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = Number(pagesInput.value);
+  const read = (readInput.value = "on" ? true : false);
+
+  addBookToLibrary(title, author, pages, read);
+  updateUi();
+
+  newBookModal.style.display = "none";
+});
 
 cancelButton.addEventListener("click", () => {
   newBookModal.style.display = "none";
-})
+});
 
 window.addEventListener("click", (event) => {
   if (event.target === newBookModal) {
     newBookModal.style.display = "none";
   }
-})
+});
